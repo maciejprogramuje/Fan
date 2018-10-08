ddb1d5751d9e7374d040bab65b7dc5312a23b2f1
maciejprogramuje <maciejprogramuje@gmail.com>
2018-10-08 12:21:15 +0200

    ver1

--- src/com/facebook/maciejprogramuje/Main.java
+++ src/com/facebook/maciejprogramuje/Main.java
@@ -6,6 +6,6 @@ class Main {
     public static void main (String[] args)
     {
         WingedFanPrinter fanPrinter = WingedFanPrinterFactory.getWingedFanPrinter(FanPrinterType.QuadrupleSymmetry);
-        fanPrinter.printFan(3, FanDirection.Clockwise);
+        fanPrinter.printFan(14, FanDirection.Clockwise);
     }
 }
--- src/com/facebook/maciejprogramuje/WingedFanPrinter.java
+++ src/com/facebook/maciejprogramuje/WingedFanPrinter.java
@@ -4,67 +4,45 @@ import java.util.ArrayList;
 import java.util.List;
 
 class WingedFanPrinter {
-    private List<String> firstQuaterLines;
-    private List<String> secondQuaterLines;
-    private List<String> thirdQuaterLines;
-    private List<String> fourthQuaterLines;
-    private int i;
+    private List<String> firstQuaterWing;
+    private List<String> secondQuaterWing;
+    private List<String> thirdQuaterWing;
+    private List<String> fourthQuaterWing;
 
     WingedFanPrinter() {
-        firstQuaterLines = new ArrayList<>();
-        secondQuaterLines = new ArrayList<>();
-        thirdQuaterLines = new ArrayList<>();
-        fourthQuaterLines = new ArrayList<>();
+        firstQuaterWing = new ArrayList<>();
+        secondQuaterWing = new ArrayList<>();
+        thirdQuaterWing = new ArrayList<>();
+        fourthQuaterWing = new ArrayList<>();
     }
 
     void printFan(int i, FanDirection fanDirection) {
-        this.i = i;
+        WingPainter wingPainter = new WingPainter(i);
 
         switch (fanDirection) {
             case Clockwise:
-                bTriangle(secondQuaterLines);
-                aTriangle(firstQuaterLines, secondQuaterLines);
-                rotate180(thirdQuaterLines, secondQuaterLines);
-                rotate180(fourthQuaterLines, firstQuaterLines);
+                secondQuaterWing = wingPainter.generateBaseTriangle();
+                firstQuaterWing = wingPainter.flipHorizontally(secondQuaterWing);
+                thirdQuaterWing = wingPainter.flipVertically(secondQuaterWing);
+                fourthQuaterWing = wingPainter.flipVertically(firstQuaterWing);
                 break;
             case CounterClockwise:
-
+                fourthQuaterWing = wingPainter.generateBaseTriangle();
+                firstQuaterWing = wingPainter.flipVertically(fourthQuaterWing);
+                secondQuaterWing = wingPainter.flipHorizontally(firstQuaterWing);
+                thirdQuaterWing = wingPainter.flipHorizontally(fourthQuaterWing);
                 break;
             default:
                 //todo = exception
         }
 
         for (int j = 0; j < i; j++) {
-            System.out.println(secondQuaterLines.get(j) + firstQuaterLines.get(j));
+            System.out.println(secondQuaterWing.get(j) + firstQuaterWing.get(j));
         }
         for (int j = 0; j < i; j++) {
-            System.out.println(thirdQuaterLines.get(j) + fourthQuaterLines.get(j));
+            System.out.println(thirdQuaterWing.get(j) + fourthQuaterWing.get(j));
         }
     }
 
-    private void bTriangle(List<String> quaterToFill) {
-        for (int j = 0; j < i; j++) {
-            StringBuilder stringBuilder = new StringBuilder();
-            for (int k = 0; k < i; k++) {
-                if (k <= j) {
-                    stringBuilder.append("*");
-                } else {
-                    stringBuilder.append(".");
-                }
-            }
-            quaterToFill.add(stringBuilder.toString());
-        }
-    }
 
-    private void aTriangle(List<String> quaterToFill, List<String> baseQuater) {
-        for (int j = i - 1; j >= 0; j--) {
-            quaterToFill.add(baseQuater.get(j));
-        }
-    }
-
-    private void rotate180(List<String> quaterToFill, List<String> baseQuater) {
-        for (int j = 0; j < i; j++) {
-            quaterToFill.add(new StringBuilder(baseQuater.get(j)).reverse().toString());
-        }
-    }
 }
